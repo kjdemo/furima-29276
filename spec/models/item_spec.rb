ca4a0f3f587908badca24d38080fe04d16030af9
kjdemo <69888834+kjdemo@users.noreply.github.com>
2020-09-21 19:21:26 +0900

    テストコード軽微な修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,37 +22,37 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Text can't be blank")
     end
 
-    it "カテゴリーの情報が空だと登録できないこと" do
+    it "カテゴリーの情報が空では登録できないこと" do
       @item.category_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
 
-    it "商品の状態の情報が空だと登録できないこと" do
+    it "商品の状態の情報が空では登録できないこと" do
       @item.status_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Status can't be blank")
     end
 
-    it "配送料の負担についての情報が空だと登録できないこと" do
+    it "配送料の負担についての情報が空では登録できないこと" do
       @item.shipping_charges_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping charges can't be blank")
     end
 
-    it "発送元の地域についての情報が空だと登録できないこと" do
+    it "発送元の地域についての情報が空では登録できないこと" do
       @item.item_address_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Item address can't be blank")
     end
 
-    it "発送までの日数についての情報が空だと登録できないこと" do
+    it "発送までの日数についての情報が空では登録できないこと" do
       @item.days_to_ship_id = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Days to ship can't be blank")
     end
 
-    it "価格が空だと登録できないこと" do
+    it "価格が空では登録できないこと" do
       @item.price = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
@@ -64,7 +64,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
 
-    it "価格のが299以下の場合登録できないこと" do
+    it "価格のが299以下のとき登録できないこと" do
       @item.price = "299"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
@@ -76,7 +76,7 @@ RSpec.describe Item, type: :model do
       expect(@item).to be_valid
     end
 
-    it "価格が9,999,999以上の時登録できないこと" do
+    it "価格が9,999,999以上のとき登録できないこと" do
       @item.price = "10000000"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
