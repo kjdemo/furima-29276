955ae7a1368a3235241e5eacdd8e66cca23255ad
kjdemo <69888834+kjdemo@users.noreply.github.com>
2020-09-29 16:29:50 +0900

    バリデーション記入

--- app/forms/user_purchase.rb
+++ app/forms/user_purchase.rb
@@ -4,17 +4,18 @@ class UserPurchase
   
   attr_accessor :postcode, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id, :user_id, :item_id, :token
 
-  # validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   with_options presence: true do
-    validates :postcode
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :phone_number, length: { minimum: 11 }, format: {with: /\A[0-9]+\z/}
   end
-
+  validates :token, presence: true
   def save
-    # binding.pry
     # 保存するのは配送先の情報とpurchaseテーブルのuser_idと_item_id,フォームオブジェクトは数値か文字しか扱えない（.id)はできない
     purchase = Purchase.create!(user_id: user_id, item_id: item_id)
     ShippingAddress.create!(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
-    # binding.pry
   end
 
 end
